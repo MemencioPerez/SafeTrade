5ef40d6025b9850e478c418985fa64ee36bc6078
MemencioPerez <memencio.perez.2004@gmail.com>
2025-11-16 14:58:46 -0400

    Implement Adventure Platform Bukkit adapter

--- src/main/java/io/github/cjcool06/safetrade/SafeTrade.java
+++ src/main/java/io/github/cjcool06/safetrade/SafeTrade.java
@@ -17,11 +17,13 @@ import io.github.cjcool06.safetrade.utils.Text;
 import me.lucko.commodore.Commodore;
 import me.lucko.commodore.CommodoreProvider;
 import me.lucko.commodore.file.CommodoreFileReader;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.minecraftforge.eventbus.api.BusBuilder;
 import net.minecraftforge.eventbus.api.IEventBus;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.spongepowered.configurate.objectmapping.guice.GuiceObjectMapperProvider;
 import org.bukkit.Bukkit;
 
@@ -34,7 +36,6 @@ import java.util.Objects;
 import java.util.logging.Logger;
 
 public class SafeTrade extends JavaPlugin {
-    public static final IEventBus EVENT_BUS = BusBuilder.builder().build();
 
     private static SafeTrade plugin;
     private EconomyService economyService;
@@ -45,9 +46,20 @@ public class SafeTrade extends JavaPlugin {
 
     private File dataFolder;
 
+    private BukkitAudiences adventure;
+
+    @NonNull
+    public BukkitAudiences adventure() {
+        if (this.adventure == null) {
+            throw new IllegalStateException("Tried to access Adventure when the plugin was disabled!");
+        }
+        return this.adventure;
+    }
+
     @Override
     public void onLoad() {
         plugin = this;
+        this.adventure = BukkitAudiences.create(this);
         Injector injector = Guice.createInjector();
         factory = injector.getInstance(GuiceObjectMapperProvider.class);
         logger = getLogger();
@@ -102,6 +114,10 @@ public class SafeTrade extends JavaPlugin {
     @Override
     public void onDisable() {
         logger.info("Executing shutdown tasks.");
+        if (this.adventure != null) {
+            this.adventure.close();
+            this.adventure = null;
+        }
         for (Trade trade : Tracker.getAllActiveTrades()) {
             trade.unloadToStorages();
         }
